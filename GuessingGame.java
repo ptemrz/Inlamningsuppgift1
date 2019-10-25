7335f4fe34b36dd06d8a77776ceea9cb9c23efab
Peter Mrazek <ptemrz@gmail.com>
2019-10-25 23:05:20 +0200

    Let the player choose the interval - Complete task 5

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,7 +1,20 @@
 public class GuessingGame {
 	
 	public static void main(String[] args) {
-		Guesser guesser = new Guesser(0,1000);
+		
+		int low = 0;
+		int high= 1000;
+		
+		if (args.length == 2) {
+			low = Integer.parseInt(args[0]);
+			high= Integer.parseInt(args[1]);
+
+		} else if (args.length > 0) {
+			System.out.println("Incorrect number of arguments to main()");
+			System.exit(1);
+		}
+		
+		Guesser guesser = new Guesser(low,high);
 		guesser.start();
 	}
 }
