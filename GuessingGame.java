db093f9124108c61733b06b77ed919f90054f7f1
Peter Mrazek <ptemrz@gmail.com>
2019-10-26 18:38:56 +0200

    Complete optional task 1
    Print usage information when called with less or more than two
    arguments.

--- GuessingGame.java
+++ GuessingGame.java
@@ -2,19 +2,23 @@ public class GuessingGame {
 	
 	public static void main(String[] args) {
 		
-		int low = 0;
-		int high= 1000;
-		
-		if (args.length == 2) {
-			low = Integer.parseInt(args[0]);
-			high= Integer.parseInt(args[1]);
-
-		} else if (args.length > 0) {
-			System.out.println("Incorrect number of arguments to main()");
+		if (!(args.length == 2)) {
+			usage();
 			System.exit(1);
 		}
 		
+		int low = Integer.parseInt(args[0]);
+		int high= Integer.parseInt(args[1]);
+		
 		Guesser guesser = new Guesser(low,high);
 		guesser.start();
+	}
+	
+	private static void usage() {
+		System.out.println("Usage: java GuessingGame low high\n"+
+				"where low is an integer for the lower bound\n"+
+				"and high is an integer for the upper bound\n"+
+				"of the interval for the numbers of the GuessingGame");
+		
 	}
 }
